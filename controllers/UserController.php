0f67f8ff4f99093121b1363cfaa6374890113de5
tasic17 <luka0492@gmail.com>
2024-11-14 23:32:48 +0100

    Vezbe 13.11.2024.

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -12,12 +12,66 @@ class UserController extends BaseController
     public function readUser()
     {
         $model = new UserModel();
-        $result = $model->get();
-        $model->mapData($result);
-        echo "<pre>";
-        var_dump($model);
-        exit;
+        $model->one("where id = 2");
 
         $this->view->render('getUser', 'main', $model);
+    }
+
+    public function readAll()
+    {
+        $model = new UserModel();
+
+        $results = $model->all("");
+
+        $this->view->render('users', 'main', $results);
+    }
+
+    public function updateUser()
+    {
+        $model = new UserModel();
+
+        $model->mapData($_GET);
+
+        $model->one("where id = $model->id");
+
+        $this->view->render('updateUser', 'main', $model);
+    }
+
+    public function processUpdateUser()
+    {
+        $model = new UserModel();
+
+        $model->mapData($_POST);
+
+        $model->update("where id = $model->id");
+
+        header("location:" . "/users");
+    }
+
+
+    public function createUser()
+    {
+
+        $model = new UserModel();
+
+        $this->view->render('createUser', 'main', $model);
+    }
+
+    public function processCreate()
+    {
+        $model = new UserModel();
+
+        $model->mapData($_POST);
+
+        $model->validate();
+
+        if ($model->errors) {
+            $this->view->render('createUser', 'main', $model);
+            exit;
+        }
+
+        $model->insert();
+
+        header("location:" . "/users");
     }
 }
